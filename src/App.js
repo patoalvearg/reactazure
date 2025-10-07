3f4758775b8e789dd391861d2b13c8bdad438b36
Patricio Alvear <palvear@ecuaprimas>
2025-10-06 19:47:46 -0500

    commit2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HOLA MUNDO ESTO ES UNA PRUEBA!
+        
         </a>
       </header>
     </div>
